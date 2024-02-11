af88738a43650985f73960b10955d9265160b036
smpol <5789000+smpol@users.noreply.github.com>
2024-02-11 15:09:47 +0100

    Update CategoryDatabaseRepository.java

--- src/main/java/com/mprzys/projekt/repository/CategoryDatabaseRepository.java
+++ src/main/java/com/mprzys/projekt/repository/CategoryDatabaseRepository.java
@@ -1,7 +1,6 @@
 package com.mprzys.projekt.repository;
 
 import com.mprzys.projekt.database.CategoryDatabase;
-import com.mprzys.projekt.database.ProductDatabase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryDatabaseRepository extends JpaRepository<CategoryDatabase, Long> {
